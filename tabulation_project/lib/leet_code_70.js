0a989cc8e4dd4cbe0da4f510e364b825171e04c3
ecbell <ebell691@gmail.com>
2022-01-07 18:09:13 +0100

    finish climbStairs leetcode

--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -2,5 +2,18 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
+  let table = new Array(n + 1).fill(0);
+  table[1] = 1
+  table[0] = 1
 
+  for (let i = 1; i < table.length; i++) {
+
+    if (2 <= n - i) {
+      table[i + 1] += table[i] + table[i - 1];
+    } else if (1 >= n - i && n - i !== 0) {
+      table[i + 1] += table[i] + table[i - 1]
+    }
+  }
+  return table[n]
+  
 }
