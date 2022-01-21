88ce638fc030997e7a28a96e3e81829c2db23a7c
ecbell <ebell691@gmail.com>
2022-01-21 11:52:08 +0100

    finish counting sort

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,19 @@
 function countingSort(arr, max) {
+    let results = [];
+    let counters = new Array(max + 1).fill(0);
 
+    for (i=0; i<arr.length; i++) {
+        counters[arr[i]]++;
+    }
+
+    for (let j=0; j<counters.length; j++) {
+        while (counters[j] > 0) {
+            results.push(j);
+            counters[j]--;
+        }
+    }
+
+    return results;
 }
 
 
