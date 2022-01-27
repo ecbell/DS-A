4abe1e8fa82bb687f4e7c445b04a733db2522c4b
ecbell <ebell691@gmail.com>
2022-01-27 23:13:47 +0100

    add readme

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,7 +1,27 @@
 function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let currElement = arr[i]; // 3
+        console.log(i);
 
+        for (var j = i - 1; currElement < arr[j] && j >=0; j--) {  // j = 1, 3 < 4 && 1 >=0
+            console.log(j);
+            arr[j + 1] = arr[j]; // arr[2] = arr[1] => 3 => 4
+            console.log(arr);
+
+        }
+
+        arr[j + 1] = currElement; // [-1, 4, 4]
+        console.log('two', arr);
+    }
+
+    return arr; 
 }
 
 module.exports = {
     insertionSort
 };
+
+
+// [-1, 4, 3, 7, 3, 7]
+
+[-1, 4]
