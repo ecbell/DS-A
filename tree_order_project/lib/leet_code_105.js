7c5bfc8bd07cc52932d1746befc64fd37929805b
ecbell <ebell691@gmail.com>
2022-02-08 11:11:36 +0100

    do leetcode problem

--- tree_order_project/lib/leet_code_105.js
+++ tree_order_project/lib/leet_code_105.js
@@ -4,6 +4,19 @@
 const { TreeNode } = require('./tree_node.js');
 
 
-function buildTree(preorder, inorder) {
+var buildTree = function (preorder, inorder) {
+  const getTree = (left = 0, right = preorder.length - 1) => {
+    if (left > right) return null;
 
-}
+    const node = preorder.shift();
+    const treeNode = new TreeNode(node);
+    const index = inorder.indexOf(node);
+
+    treeNode.left = getTree(left, index - 1);
+    treeNode.right = getTree(index + 1, right);
+    return treeNode;
+  };
+
+  return getTree();
+
+};
