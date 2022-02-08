4fe1d871e63377a0f0dcdcaa36d07825fb6519d8
ecbell <ebell691@gmail.com>
2022-02-08 12:21:50 +0100

    finish bfs

--- depth_breadth_first_project/lib/breadth_first_array.js
+++ depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,5 +1,17 @@
 function breadthFirstArray(root) {
-    
+    let queue = [ root ];
+    let resultsArr = [];
+
+    while (queue.length) {
+        let node = queue.shift();
+
+        resultsArr.push(node.val);
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return resultsArr;
 }
 
 module.exports = {
