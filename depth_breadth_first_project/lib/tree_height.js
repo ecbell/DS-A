9ad62b0cf044657ebdeb75f4d28e2b891d6eded2
ecbell <ebell691@gmail.com>
2022-02-08 12:32:15 +0100

    finish tree height

--- depth_breadth_first_project/lib/tree_height.js
+++ depth_breadth_first_project/lib/tree_height.js
@@ -1,5 +1,23 @@
 function treeHeight(root) {
+    if (root === null ) return -1;
 
+    let count = 0;
+
+    let queue = [root] 
+
+    while (queue.length) {
+
+        let node = queue.shift();
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+
+        if (node.left || node.right) {
+            count += 1;
+        }
+    }
+
+    return count;
 }
 
 
--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,5 +1,21 @@
 function treeSum(root) {
+    if (root === null) return 0;
 
+    let count = 0;
+    let queue = [root];
+
+    while (queue.length) {
+
+        let node = queue.shift();
+
+        count += node.val;
+
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+
+    return count;
 }
 
 
