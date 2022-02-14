d43d92d0c44ca301b7268c4828e59a246af9be95
ecbell <ebell691@gmail.com>
2022-02-14 23:17:13 +0100

    finish num_regions

--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,5 +1,10 @@
 function maxValue(node, visited=new Set()) {
-    
+    if (visited.has(node)) return -Infinity;
+    visited.add(node);
+    let allValues = node.neighbors.map(neighbor => maxValue(neighbor, visited));
+    return Math.max(node.val, ...allValues)
+
+
 }
 
 module.exports = {
--- graph_project/lib/num_regions.js
+++ graph_project/lib/num_regions.js
@@ -1,5 +1,24 @@
 function numRegions(graph) {
+    let visited = new Set();
+    let regions = 0;
 
+    for (let node in graph) {
+        if (_depthFirstRecur(node, graph, visited)) regions++;
+    }
+
+    return regions;
+}
+
+function _depthFirstRecur(node, graph, visited) {
+    if (visited.has(node)) return false;
+
+    visited.add(node);
+
+    graph[node].forEach(neighbor => {
+        _depthFirstRecur(neighbor, graph, visited);
+    });
+
+    return true;
 }
 
 module.exports = {
