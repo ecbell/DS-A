af5b3de723a3247a95653d3c243cda0e824f2369
ecbell <ebell691@gmail.com>
2022-02-24 15:32:33 +0100

    add dfs

--- Practice_with_graphs/graphs.js
+++ Practice_with_graphs/graphs.js
@@ -27,3 +27,48 @@ function breadthFirstSearch(start, targetVal) {
 
 // depth first search 
 
+function depthFirstSearch(start, targetVal) {
+  let stack = [start];
+  let visited = new Set();
+
+  while (stack.length > 0) {
+    let node = stack.pop();
+    if (visited.has(node)) continue;
+    visited.add(node);
+
+    if (node.val === targetVal) return node;
+    stack.push(...node.neighbors);
+  }
+}
+
+// dfs with adjacency list 
+
+function depthFirst(graph) {
+  let visited = new Set();
+
+  for (let node in graph) {
+    _depthFirstRecur(node, graph, visited);
+  }
+}
+
+function _depthFirstRecur(node, graph, visited) {
+  if (visited.has(node)) return;
+  console.log(node);
+  visited.add(node);
+
+  graph[node].forEach(neighbor => {
+    _depthFirstRecur(neighbor, graph, visited);
+  });
+}
+
+////////////////////////////////////////////////////////////////////
+//binary trees and binary search trees
+
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
